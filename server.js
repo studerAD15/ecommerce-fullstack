6aa9b2bb3807dbd2e90d3d950dc7205162d57a11
studerAD15 <adichhikaraa@gmail.com>
2026-03-03 23:15:45 +0530

    Ensure dynamic PORT

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(express.json());
 const cors = require("cors");
 app.use(cors());
 const Product = require("./models/Product");
+const PORT = process.env.PORT || 5000;
 
 /* Connect DB */
 (async () => {
@@ -100,4 +101,4 @@ app.post("/add-product", async (req, res) => {
 });
 
 /* START SERVER (ALWAYS LAST) */
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
